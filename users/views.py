395f32c90a751efd022d73442579c3a41b55efcd
Bismark <60371335+bismarkboateng@users.noreply.github.com>
2023-05-28 22:17:31 +0000

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import View
-# Create your views here.
-
-# def IndexView(request):
-#     return render(request, "index.html")
 
 
 class IndexView(View):
